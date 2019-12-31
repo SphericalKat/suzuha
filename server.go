bb0e17e32bfc4d274309d1e3548ce43fd4bb7cbe
Till Kottmann <me@deletescape.ch>
2019-12-31 01:28:18 +0100

    Index: Simplify index page to the absolute minimum
    Signed-off-by: Till Kottmann <me@deletescape.ch>

--- server.go
+++ server.go
@@ -4,39 +4,24 @@ import (
 	"github.com/deletescape/suzuha/api/anime"
 	"github.com/deletescape/suzuha/api/search"
 	"github.com/deletescape/suzuha/api/season"
-	"github.com/deletescape/suzuha/internal/config"
-	"github.com/deletescape/suzuha/pkg/entities"
 	"github.com/fasthttp/router"
 	"github.com/valyala/fasthttp"
-	"github.com/wI2L/jettison"
 	"log"
 )
 
-var indexInfo []byte
-var alive = []byte("OK")
+const indexString  = "Suzuha v0.1\ngithub.com/deletescape/suzuha"
+var indexBytes = []byte(indexString)
+var ok = []byte{'O', 'K'}
 
 func Index(ctx *fasthttp.RequestCtx) {
-	ctx.SetContentTypeBytes(config.JSONContentType)
-	_, _ = ctx.Write(indexInfo)
+	_, _ = ctx.Write(indexBytes)
 }
 
 func Alive(ctx *fasthttp.RequestCtx) {
-	_, _ = ctx.Write(alive)
+	_, _ = ctx.Write(ok)
 }
 
 func main() {
-	indexInfo, _ = jettison.Marshal(entities.IndexInfo{
-		Author:           "@deletescape",
-		Telegram:         "t.me/noneyet",
-		Version:          "0.0.1",
-		SuzuhaGo:       "0.0.1",
-		Website:          "suzuha.deletescape.ch",
-		Docs:             "suzuha.deletescape.ch/docs",
-		GitHub:           "https://github.com/deletescape/suzuha",
-		ProductionApiUrl: "https://suzuha.deletescape.cloud/api",
-		StatusUrl:        "https://status.deletescape.cloud/suzuha",
-	})
-
 	mux := router.New()
 	mux.PanicHandler = func(ctx *fasthttp.RequestCtx, i interface{}) {
 		ctx.SetStatusCode(500)
